310cca0ac4f6d010ac2c4100106a219338369c12
KIMOUGINOT <mouginotkilian@gmail.com>
2024-02-20 23:33:13 +0100

    big lines of the app

---
+++ MyApp.py
@@ -0,0 +1,12 @@
+
+
+# class MyApp():
+
+    # def __init__(self):
+    #     self.fan =
+    #     self.led =
+    #     self.transportMotor =
+    #     self.zoomMotor =
+    #     self.camera =
+
+    # def run(self):
