80c6448aae98aaabd741b6e047e082cca5aa2365
KIMOUGINOT <mouginotkilian@gmail.com>
2024-03-12 15:48:38 +0100

    class Camera

--- Camera.py
+++ Camera.py
@@ -1,17 +1,18 @@
 from picamera2 import Picamera2, Preview
 
-class Camera():
+class Camera(Picamera2):
+    def __init__(self) :
+        super().__init__()
+        camera_config = self.create_preview_configuration()
+        self.configure(camera_config)
 
-    def take_picture(image_path, image_name):
+    def take_picture(self, image_path, image_name):
         """ Take an image and save it to image_path with the label image_name
 
         Args:
             image_path (_string_):
             image_name (_string_): 
         """
-        picam2 = Picamera2()
-        camera_config = picam2.create_preview_configuration()
-        picam2.configure(camera_config)
-        picam2.start_preview(Preview.NULL)
-        picam2.start()
-        picam2.capture_file(image_path+image_name) 
+        self.start_preview(Preview.NULL)
+        self.start()
+        self.capture_file(image_path+image_name) 
