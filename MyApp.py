737ab22f841df10bd36471901ad7486269ccf709
KIMOUGINOT <mouginotkilian@gmail.com>
2024-03-13 21:45:32 +0100

    MyApp

--- MyApp.py
+++ MyApp.py
@@ -1,12 +1,16 @@
+from Camera import *
+from Fan import *
+from Led import *
 
+class MyApp():
 
-# class MyApp():
-
-    # def __init__(self):
-    #     self.fan =
-    #     self.led =
-    #     self.transportMotor =
-    #     self.camera =
-    #     init_storage()
+    def __init__(self, fan_pin, init_led_pin, transport_led_pin, end_led_pin, motor_pins, camera_motor_pins):
+        self.fan = Fan(fan_pin)
+        # self.init_led = Led(init_led_pin)
+        # self.transport_led = Led(transport_led_pin)
+        # self.end_led = Led(end_led_pin)
+        self.transportMotor = Motor(motor_pins)
+        self.camera = Camera(camera_motor_pins)
+        # self.init_storage()
 
     # def run(self):
