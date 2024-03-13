b63388e7add5854bf323140e56837dc425f417ef
KIMOUGINOT <mouginotkilian@gmail.com>
2024-03-13 10:23:51 +0100

    methode move

--- Motor.py
+++ Motor.py
@@ -1,55 +1,68 @@
 import RPi.GPIO as GPIO
-from time import sleep
-
-
-GPIO.setmode(GPIO.BCM)
-
-# Définition des broches du pilote ULN2003AN connectées au moteur
-coil_A_1_pin = 17
-coil_A_2_pin = 27
-coil_B_1_pin = 22
-coil_B_2_pin = 23
-
-# Initialisation des objets GPIO pour chaque broche
-coil_A_1 = GPIO(coil_A_1_pin)
-coil_A_2 = GPIO(coil_A_2_pin)
-coil_B_1 = GPIO(coil_B_1_pin)
-coil_B_2 = GPIO(coil_B_2_pin)
-
-# Liste des séquences pour faire tourner le moteur
-sequence = [
-    [1, 0, 0, 1],
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1]
-]
+import time
 
 class Motor():
+    def __init__(self, in1, in2, in3, in4):
+        self.step_sleep = 0.002
+        self.step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
+        self.direction = False # True for clockwise, False for counter-clockwise
+        self.step_sequence = [[1,0,0,1],
+                            [1,0,0,0],
+                            [1,1,0,0],
+                            [0,1,0,0],
+                            [0,1,1,0],
+                            [0,0,1,0],
+                            [0,0,1,1],
+                            [0,0,0,1]]
 
-    # Fonction pour faire tourner le moteur d'un pas dans une direction donnée
-    def set_step(step, direction):
-        coil_A_1.value = sequence[step][0]
-        coil_A_2.value = sequence[step][1]
-        coil_B_1.value = sequence[step][2]
-        coil_B_2.value = sequence[step][3]
+        GPIO.setmode( GPIO.BCM )
+        GPIO.setup( in1, GPIO.OUT )
+        GPIO.setup( in2, GPIO.OUT )
+        GPIO.setup( in3, GPIO.OUT )
+        GPIO.setup( in4, GPIO.OUT )
 
-    # Fonction pour faire tourner le moteur d'un nombre donné de pas dans une direction donnée
-    def move(steps, direction):
-        for _ in range(steps):
-            for step in range(8):
-                set_step(step, direction)
-                sleep(0.001)  # Délai entre chaque pas
+        # initializing
+        GPIO.output( in1, GPIO.LOW )
+        GPIO.output( in2, GPIO.LOW )
+        GPIO.output( in3, GPIO.LOW )
+        GPIO.output( in4, GPIO.LOW )
 
-    # Exemple : Faire tourner le moteur de 512 pas dans le sens horaire
-    move(512, direction=1)
+        self.motor_pins = [in1,in2,in3,in4]
+        self.motor_step_counter = 0 
 
-    # Arrêt du moteur
-    def off(self):
-        coil_A_1.off()
-        coil_A_2.off()
-        coil_B_1.off()
-        coil_B_2.off()
+    def cleanup(self):
+        GPIO.output( self.motor_pins[1], GPIO.LOW )
+        GPIO.output( self.motor_pins[2], GPIO.LOW )
+        GPIO.output( self.motor_pins[3], GPIO.LOW )
+        GPIO.output( self.motor_pins[4], GPIO.LOW )
+        GPIO.cleanup()
+
+    def move(self, steps, direction):
+            if direction:
+                self.direction = True
+            else:
+                self.direction = False
+
+            for _ in range(steps):
+                for pin in range(len(self.motor_pins)):
+                    GPIO.output(self.motor_pins[pin], self.step_sequence[self.motor_step_counter][pin])
+
+                if self.direction:
+                    self.motor_step_counter = (self.motor_step_counter - 1) % 8
+                else:
+                    self.motor_step_counter = (self.motor_step_counter + 1) % 8
+
+                time.sleep(self.step_sleep)
+
+if __name__ == "__main__":
+    # Utilisation de la classe Motor
+    in1 = 17
+    in2 = 18
+    in3 = 27
+    in4 = 22
+    motor = Motor(in1, in2, in3, in4)
+    try:
+        # Déplacer de 100 pas dans la direction spécifiée
+        motor.move(100, direction=True)  # ou False pour changer la direction
+    except KeyboardInterrupt:
+        motor.cleanup()
