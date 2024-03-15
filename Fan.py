67280e756869f85c83f4b85d8dc5ef7988a25fca
KIMOUGINOT <mouginotkilian@gmail.com>
2024-03-15 22:49:21 +0100

    new methods

--- Fan.py
+++ Fan.py
@@ -13,7 +13,13 @@ class Fan():
         self.pwm = GPIO.PWM(pin, PWM_FREQ)
         self.pwm.start(0)
         
-                
+    def start_on(self) :
+        self.pwm.ChangeDutyCycle(duty_cycle_start) # Start strong to give momentum to the fan
+        time.sleep(1)
+
+    def on(self) :   
+        self.pwm.ChangeDutyCycle(duty_cycle) # Then set the wanted duty cycle
+    
     def on_for(self, duration):
         """ Turn on the fan during the time given in parameters
 
